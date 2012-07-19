c9be964c0f666777c30946fa2b280093b4a806a9
Ian Munsie <darkstarsword@gmail.com>
2012-07-19 12:18:18 +1000

    Add git-push-wd and inotify_sync scripts
    Combined together these can be used to automatically synchronise a
    remote git tree with a local one whenever it is changed (including dirty
    modifications).
    
    Add this to .git/config on the remote tree:
    [receive]
            denyCurrentBranch = warn
    
    Then run inotify_sync.sh <remote> in a terminal in the root of the local
    git tree and any time a file is written it will automatically push the
    state of the working tree to the remote (on a branch called
    "last-deployed") and check it out.
    
    Signed-off-by: Ian Munsie <darkstarsword@gmail.com>

---
+++ git-push-wd.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+cred="[1;31;40m"
+cyellow="[1;33;40m"
+cgreen="[1;32;40m"
+
+crst="[0;37;40m"
+
+die()
+{
+	echo "${cred}$@${crst}"
+	exit 1
+}
+
+deploy_commit="last-deployed"
+
+remote="$1"; shift
+[ -z "$remote" ] && die Usage: remote_target
+
+host=$(git remote -v|grep "^$remote\\s.*(push)"|perl -pe 's|^.*ssh://(.*?)/(.*) .*$|\1|')
+rdir=/$(git remote -v|grep "^$remote\\s.*(push)"|perl -pe 's|^.*ssh://(.*?)/(.*) .*$|\2|')
+
+last_commit=$(git rev-parse HEAD)
+git commit -m git_remote_build_index_state
+index_state=$(git rev-parse HEAD)
+git add -A
+git commit -m git_remote_build_working_tree_state
+working_tree_state=$(git rev-parse HEAD)
+
+git branch -f "$deploy_commit"
+
+git reset --mixed $index_state
+git reset --soft $last_commit
+
+git push --force ${remote} ${deploy_commit} || die push failed
+ssh ${host} "cd ${rdir} && git checkout -f ${deploy_commit} && git reset --hard" || die remote build/reset failed
+
+echo "-------------------------------------------------------------------------------"
+echo "|  Deployed working tree as commit $working_tree_state   |"
+echo "-------------------------------------------------------------------------------"
---
+++ inotify_sync.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+if [ ! -d .git ]; then
+	echo Run me in the root of a git tree
+	exit 1
+fi
+
+while true; do
+	# [0-9]+ here is to ignore some weird files that vim seems to create
+	# and delete before doing it's actual save/backup dance. Now that I
+	# explicitly wait for the filesystem to settle I could probably remove
+	# it pretty safely.
+	inotifywait -e close_write -q -r --exclude '.*\.swp?x?|[0-9]+|\.git' .
+
+	while true; do
+
+		echo -n Waiting for filesystem to settle...
+		while inotifywait -t 1 -q -q -r .; [ $? -ne 2 ]; do
+			echo -n .
+			sleep 0.1
+		done
+		echo
+
+		if [ -e .git/rebase-merge ]; then
+			echo Waiting for in progress git rebase to finish...
+			inotifywait -e delete -q -r .git/rebase-merge
+			continue
+		fi
+
+		if ls .git/*.lock 2>/dev/null; then
+			echo Waiting for lock to clear...
+			inotifywait -e delete -q -r .git/*.lock
+			continue
+		fi
+
+		# TODO: Look for other signs that git is in the middle of something...
+
+	break; done
+
+	git-push-wd.sh "$@"
+	echo
+done

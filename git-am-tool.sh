381bfd9ad77a0511385d2d93fb84f34d50a556bb
Ian Munsie <darkstarsword@gmail.com>
2012-11-22 11:12:27 +1100

    git-am-tool: Allow specifying patch & optionally ignore untracked files
    This way the script can be used when we only have a simple patch file
    and not something that git-am can process.
    
    The option to ignore untracked files is because I now have a per-tree
    build.sh script in a lot of my git trees that would not be appropriate
    to commit since it just automates some of my local build setup that
    doesn't apply to anyone else.
    
    Signed-off-by: Ian Munsie <darkstarsword@gmail.com>

--- git-am-tool.sh
+++ git-am-tool.sh
@@ -2,7 +2,11 @@
 
 is_clean()
 {
-	test $(git ls-files -dmo "$toplevel"|wc -l) -eq 0
+	if [ "$ignore_untracked" = "1" ]; then
+		test $(git ls-files -dm "$toplevel"|wc -l) -eq 0
+	else
+		test $(git ls-files -dmo "$toplevel"|wc -l) -eq 0
+	fi
 }
 
 get_reject_files()
@@ -21,25 +25,52 @@ hunk_lineno()
 	awk '/^@@/ {print $2}' "$patch" | cut -d, -f 1 | cut -d- -f 2 | head -n 1
 }
 
+usage()
+{
+	echo "usage: $0 [ patch ]"
+	exit 1
+}
+
 toplevel=$(git rev-parse --show-toplevel)
 
 if [ -z "$toplevel" ]; then
 	exit 1
 fi
 
-if [ ! -f "$toplevel/.git/rebase-apply/applying" ]; then
-	echo No git-am in progress
+ignore_untracked=0
+is_am=1
+for arg in "$@"; do
+	case "$arg" in
+		'--ignore-untracked'|'-i')
+			ignore_untracked=1
+			;;
+		*)
+			if [ -n "$patch" ]; then
+				usage
+			fi
+			patch="$arg"
+			is_am=0
+			if [ ! -f "$patch" ]; then
+				echo "$patch not found"
+				usage
+			fi
+			;;
+	esac
+done
+
+if [ "$is_am" -eq "1" -a ! -f "$toplevel/.git/rebase-apply/applying" ]; then
+	echo "No git-am in progress, and no patch specified on the commandline"
 	exit 1
 fi
 
 if ! is_clean; then
-	echo "Working directory dirty"
+	echo "Working directory dirty, aborting"
 	exit 1
 fi
 
 cd "$toplevel"
 
-git apply --index --reject "$toplevel/.git/rebase-apply/patch"
+git apply --index --reject "$patch"
 
 # git apply --index doesn't seem to work with --reject when only some hunks
 # applied. I'm guessing (but haven't tested) that new/deleted files are OK:
@@ -62,6 +93,7 @@ for reject in $(get_reject_files); do
 	else
 		applied=FAIL
 		echo "WARNING: File unchanged, not cleaning up $reject"
+		# FIXME: Ask to continue, abort leaving .rej, abort cleaning up, etc.
 		echo "Press enter to continue"
 		read dummy
 	fi
@@ -69,7 +101,11 @@ done
 
 if [ "$applied" = "OK" ] && is_clean; then
 	echo
-	echo "All files resolved, you should continue by running 'git am --resolved'"
+	if [ "$is_am" -eq "1" ]; then
+		echo "All files resolved, you should continue by running 'git am --resolved'"
+	else
+		echo "All files resolved, you should now commit the result"
+	fi
 	# git am --resolved
 else
 	echo

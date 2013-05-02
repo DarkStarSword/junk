b5363824ad841d0ff2d58c65d1670e613b83d08c
Ian Munsie <darkstarsword@gmail.com>
2013-05-02 13:04:26 +1000

    check-title.py: Always capitalise first + last word & refactor
    In English, the first and last words of a title should always be
    capitalised regardless of any other rule. E.g. The "On" in "On the
    Meaning of Life" is supposed to be capitalised.
    
    Also refactor the code while I'm at it.
    
    Signed-off-by: Ian Munsie <darkstarsword@gmail.com>

--- check-title.py
+++ check-title.py
@@ -9,31 +9,48 @@ dont_capitalise = [(re.compile(r'(?<!\w)%s(?!\w)' % re.escape(word.title())), wo
 	'while', 'even though', 'of', 'in', 'on', 'with', 'as', 'by', 'at',
 	'from', 'vs.']]
 
+def first_middle_last(title):
+	first = title.strip()
+	try:
+		(first, last) = first.split(' ', 1)
+	except ValueError:
+		last = ''
+	try:
+		(middle, last) = last.rsplit(' ', 1)
+	except ValueError:
+		middle = ''
+	return (first, middle, last)
+
+def rejoin_title(first, middle, last):
+	return ' '.join(filter(None, [first, middle, last]))
+
+def _better_title(title, title_func):
+	(first, middle, last) = map(title_func, first_middle_last(title))
+	for (exp, word) in dont_capitalise:
+		middle = exp.sub(word, middle)
+	return rejoin_title(first, middle, last)
+
 def better_title_nolower(title):
 	'''
 	Like str.title, but doesn't capitalise certain words and doesn't change
 	letters that are already capitals to lower case.
 	'''
-	tmp = title.split(' ')
-	for (i, word) in enumerate(tmp):
-		tmp[i] = word[0].upper() + word[1:]
-	tmp = ' '.join(tmp)
-	for (exp, word) in dont_capitalise:
-		tmp = exp.sub(word, tmp)
-	return tmp
+	def title_nolower(title):
+		if not title:
+			return title
+		return ' '.join([ word[0].upper() + word[1:] for word in title.split(' ') ])
+	return _better_title(title, title_nolower)
 
 def better_title(title):
 	'''
 	Like str.title, but doesn't capitalise certain words.
 	'''
-	tmp = title.title()
-	for (exp, word) in dont_capitalise:
-		tmp = exp.sub(word, tmp)
-	return tmp
+	return _better_title(title, str.title)
 
 def main():
 	import sys
 	print better_title(' '.join(sys.argv[1:]))
+	# print better_title_nolower(' '.join(sys.argv[1:]))
 
 if __name__ == '__main__':
 	main()

8274902df657a7b3f0763a6a713b386cc80a188b
Ian Munsie <darkstarsword@gmail.com>
2013-07-01 12:33:28 +1000

    i3 config/thinkpad: Put tray on left monitor at work
    Set the tray to be on the primary output, and set the left monitor as
    the primary.

--- config/home/.i3/local/thinkpad/work.sh
+++ config/home/.i3/local/thinkpad/work.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-xrandr --output LVDS-0 --off           --output DP-1 --mode 1600x1200 --output DP-2 --off
-xrandr --output LVDS-0 --off           --output DP-1 --mode 1600x1200 --output DP-2 --mode 1600x1200 --right-of DP-1
+xrandr --output LVDS-0 --off           --output DP-1 --mode 1600x1200 --primary --output DP-2 --off
+xrandr --output LVDS-0 --off           --output DP-1 --mode 1600x1200 --primary --output DP-2 --mode 1600x1200 --right-of DP-1
 dispwin -d 2 -I /home/ian/colorhug/results/r_lut/thinkvision_r_lut.icc
 dispwin -d 1 -I /home/ian/colorhug/results/left/thinkvision_l.icc

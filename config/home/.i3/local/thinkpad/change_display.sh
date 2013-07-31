9bf66403f82a803af3c2eeaf980af7486ae6398e
Ian Munsie <darkstarsword@gmail.com>
2013-07-31 16:51:43 +1000

    i3/thinkpad: add 640x480 projector script

--- config/home/.i3/local/thinkpad/change_display.sh
+++ config/home/.i3/local/thinkpad/change_display.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-display=$(echo -e "internal\nwork\nhome\nprojector" | dmenu -b)
+display=$(echo -e "internal\nwork\nhome\nprojector\nprojector-640x480" | dmenu -b)
 script=~/.i3/local/thinkpad/"$display".sh
 if [ -x "$script" ]; then
 	"$script"

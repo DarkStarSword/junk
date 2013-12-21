640fd8cdab7179786887b4bfe095c8d01efd23a8
Ian Munsie <darkstarsword@gmail.com>
2013-12-22 02:45:13 +1100

    Userscript to add a refresh button to the Steam market

---
+++ steam/steam_market_refresh.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name        Steam Community Market Refresh Button
+// @namespace   DarkStarSword
+// @include     http://steamcommunity.com/market/listings/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+document.getElementById('market_buynow_dialog_accept_ssa').checked = true;
+
+//refresh_url = 'javascript:s=g_oSearchResults;s.m_iCurrentPage=1;s.GoToPage(0)'
+// refresh_url = 'javascript:document.getElementsByClassName("market_listing_right_cell market_listing_action_buttons")[0].innerHTML="Refreshing...";s=g_oSearchResults;s.m_iCurrentPage=1;s.GoToPage(0)';
+refresh_url = 'javascript:document.getElementById("searchResultsTable").scrollIntoView(true); document.getElementsByClassName("market_listing_right_cell market_listing_action_buttons")[0].innerHTML="Refreshing...";s=g_oSearchResults;s.m_iCurrentPage=1;s.GoToPage(0)';
+
+elem_new = document.createElement('a');
+elem_new.href = refresh_url;
+elem_new.appendChild(document.createTextNode('REFRESH!!!'));
+
+elem_div = document.createElement('div');
+elem_div.align = 'right';
+elem_div.appendChild(elem_new);
+
+elem_after = document.getElementById('searchResultsRows');
+elem_after.parentNode.insertBefore(elem_div, elem_after);
+
+elem_div.scrollIntoView(true); // someone scrolls after us :(

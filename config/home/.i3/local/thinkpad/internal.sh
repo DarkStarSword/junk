4e4b5543cc37fb3c421bbe06eec53c4c67ebc54f
Ian Munsie <darkstarsword@gmail.com>
2014-07-24 17:55:37 +1000

    DPI adjustments for my Thinkpad

--- config/home/.i3/local/thinkpad/internal.sh
+++ config/home/.i3/local/thinkpad/internal.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 xrandr --output LVDS1 --mode 1366x768 --output HDMI2 --off            --output HDMI3 --off --output VGA1 --off
+# Set DPI to 96 - it's not accurate, but the "correct" DPI of ~125 seems to
+# scale my fonts way too high (even compared to an accurate DPI on my external
+# monitors), looks horrible, causes issues for applications that don't scale
+# all their UI along with the DPI (e.g. the tab bar in Firefox), and windows
+# moved between monitors of differing DPIs don't get adjusted.
+xrandr --dpi 96
 
 # Need to calibrate new laptop display
 dispwin -c
--- config/home/.i3/local/thinkpad/work.sh
+++ config/home/.i3/local/thinkpad/work.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 xrandr --output LVDS1 --off           --output HDMI2 --mode 1600x1200 --primary --output HDMI3 --mode 1600x1200 --right-of HDMI2 --output VGA1 --off
+xrandr --fbmm 410x308 # Set correct DPI (this seems to need the dimensions of just one monitor?)
 dispwin -d 2 -I /home/ian/colorhug/results/r_lut/thinkvision_r_lut.icc
 dispwin -d 1 -I /home/ian/colorhug/results/left/thinkvision_l.icc

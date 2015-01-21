bd4992d6567bef7addf4a67a2db1dc8d7e0f0b25
Ian Munsie <darkstarsword@gmail.com>
2015-01-21 13:53:41 +1100

    Thinkpad config: Alter work monitor layout

--- config/home/.i3/local/thinkpad/work3.sh
+++ config/home/.i3/local/thinkpad/work3.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-xrandr --output HDMI2 --mode 1600x1200 --primary --output HDMI3 --mode 1600x1200 --right-of HDMI2 --output LVDS1 --mode 1366x768 --right-of HDMI3 --output VGA1 --off
+# xrandr --output HDMI2 --mode 1600x1200 --primary --output HDMI3 --mode 1600x1200 --right-of HDMI2 --output LVDS1 --mode 1366x768 --right-of HDMI3 --output VGA1 --off
+xrandr --output LVDS1 --mode 1366x768 --primary --output HDMI2 --mode 1600x1200 --right-of LVDS1 --output HDMI3 --mode 1600x1200 --right-of HDMI2 --output VGA1 --off
 dispwin -d 2 -I /home/ian/colorhug/results/r_lut/thinkvision_r_lut.icc
 dispwin -d 1 -I /home/ian/colorhug/results/left/thinkvision_l.icc

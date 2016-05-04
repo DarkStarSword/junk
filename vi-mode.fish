c6133ff4971cc4d04b11329b07abab22a076f61d
Ian Munsie <darkstarsword@gmail.com>
2016-05-04 14:33:57 +1000

    vi-mode.fish: Fix replace mode by removing old workaround
    Originally I had to put a hack in replace mode to make it work,
    presumably to work around a bug in fish. This no longer works in fish
    2.1.0 because presumably the bug was fixed, so remove the workaround and
    use the correct procedure.

--- vi-mode.fish
+++ vi-mode.fish
@@ -366,11 +366,7 @@ function __vi_mode_replace
 	bind --erase --all
 	__vi_mode_common
 
-	# backward-char should happen last, but only works if specified first
-	# (guess I should dig through the C code and figure out what is going
-	# on):
-	# __vi_mode_bind_all "commandline -f delete-char; commandline -i %k; commandline -f backward-char; __vi_mode_normal"
-	__vi_mode_bind_all "__vi_mode_save_cmdline; commandline -f backward-char delete-char; commandline -i %k; __vi_mode_normal"
+	__vi_mode_bind_all "commandline -f delete-char; commandline -i %k; commandline -f backward-char; __vi_mode_normal"
 
 	if functions -q vi_mode_user
 		vi_mode_user replace
